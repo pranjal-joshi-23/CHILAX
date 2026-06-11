66e64e4b4a2c97d856c72bb02709d0b5e9785e55
Pranjal Joshi <pranjaljoshi2004@gmail.com>
2026-06-11 23:39:17 +0530

    Made minor changes in keyboard controlls. Mainly improved its handling.

--- src/chilax_controlls/chilax_controlls/keyboard_controlles_mapping.py
+++ src/chilax_controlls/chilax_controlls/keyboard_controlles_mapping.py
@@ -3,6 +3,7 @@ import rclpy
 from rclpy.node import Node
 from std_msgs.msg import String
 from geometry_msgs.msg import Twist
+import time
 
 class MyNode(Node):
     def __init__(self):
@@ -10,23 +11,51 @@ class MyNode(Node):
         self.key_subscriber = self.create_subscription(String, "keyboard_input", self.callback_key_press, 10)
         self.velocity_publisher = self.create_publisher(Twist, "velocity_control", 10)
 
+        self.velocity = Twist()
+        self.timer_ = self.create_timer(0.01, self.callback_publisher)
+        self.last_key_time = 0.0
+
     def callback_key_press(self, data: String):
         key = data.data
 
-        velocity = Twist()
-
         if key == "w":
-            velocity.linear.x = 6.0
+            self.velocity.linear.x = 6.0
         elif key == "W":
-            velocity.linear.x = 8.0
-        elif key == "s":
-            velocity.linear.x = -6.0
-        elif key == "S":
-            velocity.linear.x = -8.0
-        else:
-            velocity.linear.x = 0.0
+            self.velocity.linear.x = 8.0
+        elif key == "x":
+            self.velocity.linear.x = -6.0
+        elif key == "X":
+            self.velocity.linear.x = -8.0
+        elif key == "a":
+            self.velocity.angular.z = 6.0
+        elif key == "A":
+            self.velocity.angular.z = 8.0
+        elif key == "d":
+            self.velocity.angular.z = -6.0
+        elif key == "D":
+            self.velocity.angular.z = -8.0
+        elif key == "q":
+            self.velocity.linear.x = 6.0
+            self.velocity.angular.z = 6.0
+        elif key == "z":
+            self.velocity.linear.x = -6.0
+            self.velocity.angular.z = 6.0
+        elif key == "c":
+            self.velocity.linear.x = -6.0
+            self.velocity.angular.z = -6.0
+        elif key == "e":
+            self.velocity.linear.x = 6.0
+            self.velocity.angular.z = -6.0
+        # else:
+        #     self.velocity.linear.x = 0.0
+        #     self.velocity.angular.z = 0.0
+        self.last_key_time = time.time()
 
-        self.velocity_publisher.publish(velocity)
+    def callback_publisher(self):
+        if time.time() - self.last_key_time > 0.01:
+            self.velocity.linear.x = 0.0
+            self.velocity.angular.z = 0.0
+        self.velocity_publisher.publish(self.velocity)
 
 def main(args=None):
     rclpy.init(args=args)
